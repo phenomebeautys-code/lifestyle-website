8c52c1252105d17bd3eb88d8a027036157112595
phenomebeautys-code <phenomebeautys@gmail.com>
2026-06-12 23:07:48 +0200

    Update shop.js

--- shop.js
+++ shop.js
@@ -322,7 +322,11 @@ function addToCartFromDetail(pid) {
     const orig = btn.innerHTML;
     btn.innerHTML = '<svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg> Added to Cart';
     btn.disabled = true;
-    setTimeout(() => { btn.innerHTML = orig; btn.disabled = false; }, 1500);
+    setTimeout(() => {
+      btn.innerHTML = orig;
+      btn.disabled = false;
+     closeProductDetail();
+   }, 1500);
   }
 }
 
